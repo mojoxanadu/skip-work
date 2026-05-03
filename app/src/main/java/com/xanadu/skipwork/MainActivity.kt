096a8dec337181ed906f150466942bf322ad8312
Mojo <mojo.xanadu.2@gmail.com>
2026-05-03 16:19:05 -0700

    Fix Kotlin syntax error in MainActivity

--- app/src/main/java/com/xanadu/skipwork/MainActivity.kt
+++ app/src/main/java/com/xanadu/skipwork/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.xanadu.skipwork
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import android.view.Gravity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState: Bundle?)
-        // Hide the action bar for that clean "Zelda III" title screen
-        supportActionBar?.hide() 
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        
+        // Simple programmatic UI for our alpha test
+        val textView = TextView(this)
+        textView.text = "SKIP WORK\n[Alpha Build]"
+        textView.textSize = 32f
+        textView.gravity = Gravity.CENTER
+        
+        setContentView(textView)
     }
 }
