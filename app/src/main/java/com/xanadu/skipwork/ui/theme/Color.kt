2c2ff56f7bf38c92545625c3a8dfd3edfa52fbf5
mojoxanadu <mojo.xanadu.2@gmail.com>
2026-05-07 11:28:40 -0700

    Add Skip Work color palette

---
+++ app/src/main/java/com/xanadu/skipwork/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.xanadu.skipwork.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val Beige = Color(0xFFD4AF37)
+val DarkGray = Color(0xFF1F1F1F)
+val LightGray = Color(0xFF404040)
+val Red = Color(0xFFE63946)
