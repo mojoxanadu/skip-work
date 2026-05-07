f490b66823f7b49a473cef4627fedfb94cbd7775
mojoxanadu <mojo.xanadu.2@gmail.com>
2026-05-07 11:22:26 -0700

    Add AppState enum

---
+++ app/src/main/java/com/xanadu/skipwork/AppState.kt
@@ -0,0 +1,17 @@
+package com.xanadu.skipwork
+
+/**
+ * Enum representing all possible app states/screens in the Skip Work game.
+ */
+enum class AppState {
+    APP_LOADING,
+    MAIN_MENU,
+    START_OVER_CONFIRM,
+    HELP,
+    CREDITS,
+    FLOOR_LOADING,
+    GAME_ACTIVE,
+    LOST_LIFE,
+    STORE,
+    SETTINGS
+}
